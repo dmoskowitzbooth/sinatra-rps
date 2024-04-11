16ac5f478535061a26f9881c3fefd122f7daf5f7
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-04-11 16:10:50 +0000

    3

--- app.rb
+++ app.rb
@@ -10,14 +10,14 @@ end
 get("/app/paper") do
   rps=" rock paper scissors".split
   @play=rps.sample
-  @outcome="we played paper, and they played #{@play}."
+  @outcome="They played #{@play}!"
   erb(:paper)
 end
 
 get("/app/rock") do
   rps=" rock paper scissors".split
   @play=rps.sample
-  @outcome="we played rock, and they played #{@play}."
+  @outcome="They played #{@play}!"
   erb(:rock)
 end
 
@@ -25,6 +25,6 @@ get("/app/scissors") do
  
   rps=" rock paper scissors".split
   @play=rps.sample
-  @outcome="we played scissors, and they played #{@play}."
+  @outcome="They played #{@play}!"
   erb(:scissors)
 end
