895137c36a6b6ac09ea49bde9fc5662d54f0d98f
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-04-11 15:56:55 +0000

    2

--- app.rb
+++ app.rb
@@ -8,16 +8,18 @@ erb(:home)
 end
 
 get("/app/paper") do
-  erb(:paper)
   pp "welcome to paper!"
+  erb(:paper)
 end
 
 get("/app/rock") do
-  erb(:rock)
+  
   pp "welcome to rock!"
+  erb(:rock)
 end
 
 get("/app/scissors") do
-  erb(:scissors)
+ 
   pp "welcome to scissors!"
+  erb(:scissors)
 end
